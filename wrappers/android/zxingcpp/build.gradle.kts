4ea37ce4c3508cf9a28cd34a8171a88bb2b675fe
Nick <nift4@protonmail.com>
2025-08-20 21:15:58 +0200

    update urls

--- wrappers/android/zxingcpp/build.gradle.kts
+++ wrappers/android/zxingcpp/build.gradle.kts
@@ -101,8 +101,8 @@ publishing {
         maven {
             name = "sonatype"
 
-            val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-            val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+            val releasesRepoUrl = "https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/"
+            val snapshotsRepoUrl = "https://ossrh-staging-api.central.sonatype.com/content/repositories/snapshots/"
             setUrl(if (version.toString().endsWith("-SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
             if (!version.toString().endsWith("-SNAPSHOT") && publishSnapshot == "true")
                 throw IllegalStateException("$version does not end with -SNAPSHOT but $publishSnapshot is true")
